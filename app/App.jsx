753a3f477b8837748328822a6597b54086900ca5
HaoDuong125 <haoduong125@gmail.com>
2017-09-19 08:59:56 +0700

    fit bug done and run on localhost

---
+++ app/App.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+class App extends Component {
+  constructor()
+  {
+    super ();
+    this.state={
+      data:[],
+    }
+  }
+  componentDidMount()
+  {
+    fetch('https://api.coinmarketcap.com/v1/ticker/?limit=10').
+    then((Response)=>Response.json()).
+    then((findresponse)=>
+        {
+      console.log(findresponse)
+      this.setState({
+      data:findresponse,
+      })
+      })
+   
+  }
+  render()
+  {
+    return(
+    <div>
+      {
+
+      this.state.data.map((dynamicData, key)=>
+       <div>
+       
+       <p> Name = {dynamicData.name} <br /> </p>
+       <p>  ID = {dynamicData.id} <br /> </p>
+        <p>  Symbol = {dynamicData.symbol} <br /> </p>
+        <p> -------------------------   </p>
+        </div>
+      )
+    }
+      </div>
+      )
+  }
+}
+export default App;
